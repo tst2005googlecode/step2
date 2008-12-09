4508592b15411275d7d7cc5ca08f86a170b97fb4
dirk.balfanz@gmail.com <dirk.balfanz@gmail.com@c427a029-b451-0410-ae65-13123b152969>
2008-12-09 19:36:32 +0000

    just whitespaces.

--- code/java/trunk/common/src/main/java/com/google/step2/openid/ax2/ValidateRequest.java
+++ code/java/trunk/common/src/main/java/com/google/step2/openid/ax2/ValidateRequest.java
@@ -4,15 +4,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 
 package com.google.step2.openid.ax2;
@@ -26,14 +26,14 @@ import org.openid4java.message.ax.StoreRequest;
 import java.util.Iterator;
 
 /**
- * Implementation of a proposed "validate" attribute exchange request 
- * 
+ * Implementation of a proposed "validate" attribute exchange request
+ *
  * @author Steve Weis (steveweis@gmail.com)
  */
 public class ValidateRequest extends StoreRequest {
   private static final Logger LOG = Logger.getLogger(ValidateRequest.class);
   private static final boolean DEBUG = LOG.isDebugEnabled();
-  
+
   @Override
   public String getTypeUri() {
     return AxMessage2.OPENID_NS_AX_FINAL;
@@ -45,10 +45,10 @@ public class ValidateRequest extends StoreRequest {
 
     if (DEBUG) LOG.debug("Created empty validate request.");
   }
-  
+
   public ValidateRequest(ParameterList params) {
     super(params);
-    
+
     _parameters.set(
         new Parameter(AxMessage2.MODE, Mode.VALIDATE_REQUEST.toString()));
 
